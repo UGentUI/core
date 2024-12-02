f78dcbb2a768d98e94061fabcd0e4ae4d881d53f
Jago Pauwels <jago.pauwels@ugent.be>
2024-12-02 15:45:55 +0100

    Prettier Config

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,27 @@
 // eslint.config.js
-import eslintPlugin from "@typescript-eslint/eslint-plugin";
-import parser from "@typescript-eslint/parser";
+import eslintPlugin from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default [
   {
-    files: ["src/**/*.{ts,tsx}"],
+    files: ['src/**/*.{ts,tsx}'],
     languageOptions: {
       parser,
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       parserOptions: {
-        project: "./tsconfig.json",
-      },
+        project: './tsconfig.json'
+      }
     },
     plugins: {
-      "@typescript-eslint": eslintPlugin,
+      '@typescript-eslint': eslintPlugin
     },
     rules: {
       ...eslintPlugin.configs.recommended.rules,
-      ...eslintPlugin.configs["recommended-requiring-type-checking"].rules,
+      ...eslintPlugin.configs['recommended-requiring-type-checking'].rules
     },
-    ignores: ["node_modules/**"],
+    ignores: ['node_modules/**']
   },
+  eslintPluginPrettierRecommended
 ];
