2a598b7a52ecea9821317cde4005ee7a19636122
Dirk Germonpré <dirk.germonpre@ugent.be>
2024-12-18 15:50:27 +0100

    PROJ0484-24 Card Stories Ontwikkelen eerste versie

--- stories/components/UGCard.stories.ts
+++ stories/components/UGCard.stories.ts
@@ -4,7 +4,41 @@ import '/lib/components/card';
 
 const meta: Meta = {
   title: 'Components/Card',
-  component: 'ug-card'
+  component: 'ug-card',
+  parameters: {
+    docs: {
+      subtitle: 'Cards can be used to group related subjects in a container.'
+    }
+  },
+  argTypes: {
+    headerSlot: {
+      name: 'header',
+      description: 'An optional header for the card.',
+      control: 'text',
+      table: {
+        category: 'slots',
+        defaultValue: { summary: undefined }
+      }
+    },
+    footerSlot: {
+      name: 'footer',
+      description: 'An optional footer for the card.',
+      control: 'text',
+      table: {
+        category: 'slots',
+        defaultValue: { summary: undefined }
+      }
+    },
+    imageSlot: {
+      name: 'image',
+      description: 'An optional image to render at the start of the card.',
+      control: 'text',
+      table: {
+        category: 'slots',
+        defaultValue: { summary: undefined }
+      }
+    }
+  }
 };
 
 export default meta;
@@ -21,5 +55,48 @@ export const Card: Story = {
           max-width: 300px;
         }
       </style>`;
+  }
+};
+
+export const CardWithHeader: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: `Headers can be used to display titles and more.`
+      },
+      source: {
+        format: true
+      }
+    }
+  },
+  render: () => {
+    return html`<ug-card class="card-header">
+        <div slot="header">
+          Header Title
+          <ug-icon-button name="gear" label="Settings"></ug-icon-button>
+        </div>
+
+        This card has a header. You can put all sorts of things in it!
+      </ug-card>
+
+      <style>
+        .card-header {
+          max-width: 300px;
+        }
+
+        .card-header [slot='header'] {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+        }
+
+        .card-header h3 {
+          margin: 0;
+        }
+
+        .card-header ug-icon-button {
+          font-size: var(--ug-font-size-medium);
+        }
+      </style> `;
   }
 };
