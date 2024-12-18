fe2a70d625bf12adcbf088b5c11b88dfba7c9d8b
mbardady <maryna.bardadym@ugent.be>
2024-12-18 09:03:28 +0100

    stories/UGTab parameters

--- stories/components/UGTab.stories.ts
+++ stories/components/UGTab.stories.ts
@@ -1,10 +1,60 @@
 import { html } from 'lit';
 import type { Meta, StoryObj } from '@storybook/web-components';
 import '/lib/components/tab';
+import { action } from '@storybook/addon-actions';
 
 const meta: Meta = {
   title: 'Components/Tab',
-  component: 'ug-tab'
+  component: 'ug-tab',
+
+  argTypes: {
+    panel: {
+      name: 'Panel',
+      description:
+        'The name of the tab panel this tab is associated with. The panel must be located in the same tab group.',
+      control: { type: 'text' }, // Input text box
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: "''" }
+      }
+    },
+    active: {
+      name: 'Active',
+      description: 'Draws the tab in an active state.',
+      control: { type: 'boolean' }, // Toggle switch
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' }
+      }
+    },
+    closable: {
+      name: 'Closable',
+      description: 'Makes the tab closable and shows a close button.',
+      control: { type: 'boolean' }, // Toggle switch
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' }
+      }
+    },
+    disabled: {
+      name: 'Disabled',
+      description: 'Disables the tab and prevents selection.',
+      control: { type: 'boolean' }, // Toggle switch
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' }
+      }
+    },
+    updateComplete: {
+      name: 'Update Complete',
+      description:
+        'A read-only promise that resolves when the component has finished updating.',
+      control: false, // Read-only, so no control
+      table: {
+        type: { summary: 'Promise<void>' }
+      }
+    }
+  }
 };
 
 export default meta;
@@ -12,7 +62,19 @@ export default meta;
 type Story = StoryObj;
 
 export const Tab: Story = {
-  render: () => {
-    return html`<ug-tab></ug-tab>`;
+  args: {
+    panel: '',
+    active: false,
+    closable: false,
+    disabled: false
+  },
+  render: (args) => {
+    return html` <ug-tab
+      panel="${args.panel}"
+      ?active="${args.active}"
+      ?closable="${args.closable}"
+      ?disabled="${args.disabled}"
+    >
+    </ug-tab>`;
   }
 };
