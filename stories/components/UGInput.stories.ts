606c4cc968bea8149cc1fd4930127e5191062e4f
mbardady <maryna.bardadym@ugent.be>
2025-01-20 12:29:57 +0100

    PROJ0484-39 stories/UGInput
    First draft: Argtypes and first default story

--- stories/components/UGInput.stories.ts
+++ stories/components/UGInput.stories.ts
@@ -2,9 +2,581 @@ import { html } from 'lit';
 import type { Meta, StoryObj } from '@storybook/web-components';
 import '/lib/components/input';
 
+function removeDefaultAttributes(code: string): string {
+  return code
+    .replace(
+      /\s*(name=""|size="medium"|placeholder=""|label=""|help-text=""|placeholder="")/g,
+      ''
+    )
+    .replace(/\s* hoist=""/g, ' hoist')
+    .replace(/\s* multiple=""/g, ' multiple')
+    .replace(/\s* clearable=""/g, ' clearable')
+    .replace(/\s* disabled=""/g, ' disabled')
+    .replace(/\s* required=""/g, ' required');
+}
+
 const meta: Meta = {
   title: 'Components/Input',
-  component: 'ug-input'
+  component: 'ug-input',
+
+  parameters: {
+    docs: {
+      subtitle: 'This component works with standard <form> elements',
+      source: {
+        transform: (code: string) => removeDefaultAttributes(code),
+        format: true
+      }
+    }
+  },
+  argTypes: {
+    type: {
+      description:
+        'The type of input. Works the same as a native <code>&lt;input&gt;</code> element, but only a subset of types are supported.',
+      control: { type: 'select' },
+      options: [
+        'date',
+        'datetime-local',
+        'email',
+        'number',
+        'password',
+        'search',
+        'tel',
+        'text',
+        'time',
+        'url'
+      ],
+      defaultValue: 'text',
+      table: {
+        category: 'Attributes: Basic Input Behavior',
+        type: {
+          summary:
+            "'date' | 'datetime-local' | 'email' | 'number' | 'password' | 'search' | 'tel' | 'text' | 'time' | 'url'"
+        },
+        defaultValue: { summary: "'text'" }
+      }
+    },
+    name: {
+      description:
+        'The name of the input, submitted as a name/value pair with form data.',
+      control: { type: 'text' },
+      defaultValue: '',
+      table: {
+        category: 'Attributes: Basic Input Behavior',
+        type: { summary: 'string' },
+        defaultValue: { summary: "''" }
+      }
+    },
+    value: {
+      description:
+        'The current value of the input, submitted as a name/value pair with form data.',
+      control: { type: 'text' },
+      defaultValue: '',
+      table: {
+        category: 'Attributes: Basic Input Behavior',
+        type: { summary: 'string' },
+        defaultValue: { summary: "''" }
+      }
+    },
+    defaultValue: {
+      description:
+        'The default value of the form control. Primarily used for resetting the form control.',
+      control: { type: 'text' },
+      defaultValue: '',
+      table: {
+        category: 'Attributes: Basic Input Behavior',
+        type: { summary: 'string' },
+        defaultValue: { summary: "''" }
+      }
+    },
+    size: {
+      description: 'The input’s size.',
+      control: { type: 'select' },
+      options: ['small', 'medium', 'large'],
+      defaultValue: 'medium',
+      table: {
+        category: 'Attributes: Visual Appearance',
+        type: { summary: "'small' | 'medium' | 'large'" },
+        defaultValue: { summary: "'medium'" }
+      }
+    },
+    filled: {
+      description: 'Draws a filled input.',
+      control: { type: 'boolean' },
+      defaultValue: false,
+      table: {
+        category: 'Attributes: Visual Appearance',
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' }
+      }
+    },
+    pill: {
+      description: 'Draws a pill-style input with rounded edges.',
+      control: { type: 'boolean' },
+      defaultValue: false,
+      table: {
+        category: 'Attributes: Visual Appearance',
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' }
+      }
+    },
+    label: {
+      description:
+        'The input’s label. If you need to display HTML, use the label slot instead.',
+      control: { type: 'text' },
+      defaultValue: '',
+      table: {
+        category: 'Attributes: Visual Appearance',
+        type: { summary: 'string' },
+        defaultValue: { summary: "''" }
+      }
+    },
+    helpText: {
+      description:
+        'The input’s help text. If you need to display HTML, use the help-text slot instead.',
+      control: { type: 'text' },
+      defaultValue: '',
+      table: {
+        category: 'Attributes: Visual Appearance',
+        type: { summary: 'string' },
+        defaultValue: { summary: "''" }
+      }
+    },
+    clearable: {
+      description: 'Adds a clear button when the input is not empty.',
+      control: { type: 'boolean' },
+      defaultValue: false,
+      table: {
+        category: 'Attributes: Form and Accessibility',
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' }
+      }
+    },
+    disabled: {
+      description: 'Disables the input.',
+      control: { type: 'boolean' },
+      defaultValue: false,
+      table: {
+        category: 'Attributes: Visual Appearance',
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' }
+      }
+    },
+    placeholder: {
+      description:
+        'Placeholder text to show as a hint when the input is empty.',
+      control: { type: 'text' },
+      defaultValue: '',
+      table: {
+        category: 'Attributes: Basic Input Behavior',
+        type: { summary: 'string' },
+        defaultValue: { summary: "''" }
+      }
+    },
+    readonly: {
+      description: 'Makes the input readonly.',
+      control: { type: 'boolean' },
+      defaultValue: false,
+      table: {
+        category: 'Attributes: Visual Appearance',
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' }
+      }
+    },
+    passwordToggle: {
+      description:
+        'Adds a button to toggle the password’s visibility. Only applies to password types.',
+      control: { type: 'boolean' },
+      defaultValue: false,
+      table: {
+        category: 'Attributes: Visual Appearance',
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' }
+      }
+    },
+    passwordVisible: {
+      description:
+        'Determines whether or not the password is currently visible. Only applies to password input types.',
+      control: { type: 'boolean' },
+      defaultValue: false,
+      table: {
+        category: 'Attributes: Visual Appearance',
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' }
+      }
+    },
+    noSpinButtons: {
+      description:
+        'Hides the browser’s built-in increment/decrement spin buttons for number inputs.',
+      control: { type: 'boolean' },
+      defaultValue: false,
+      table: {
+        category: 'Attributes: Visual Appearance',
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' }
+      }
+    },
+    form: {
+      description:
+        'Allows you to place the form control outside of a form and associate it with the form that has this id.',
+      control: { type: 'text' },
+      defaultValue: '',
+      table: {
+        category: 'Attributes: Form and Accessibility',
+        type: { summary: 'string' },
+        defaultValue: { summary: "''" }
+      }
+    },
+    required: {
+      description: 'Makes the input a required field.',
+      control: { type: 'boolean' },
+      defaultValue: false,
+      table: {
+        category: 'Attributes: validation',
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' }
+      }
+    },
+    pattern: {
+      description: 'A regular expression pattern to validate input against.',
+      control: { type: 'text' },
+      defaultValue: undefined,
+      table: {
+        category: 'Attributes: validation',
+        type: { summary: 'string' },
+        defaultValue: { summary: '-' }
+      }
+    },
+    minlength: {
+      description: 'The minimum length of input that will be considered valid.',
+      control: { type: 'number' },
+      defaultValue: undefined,
+      table: {
+        category: 'Attributes: validation',
+        type: { summary: 'number' },
+        defaultValue: { summary: '-' }
+      }
+    },
+    maxlength: {
+      description: 'The maximum length of input that will be considered valid.',
+      control: { type: 'number' },
+      defaultValue: undefined,
+      table: {
+        category: 'Attributes: validation',
+        type: { summary: 'number' },
+        defaultValue: { summary: '-' }
+      }
+    },
+    min: {
+      description:
+        'The input’s minimum value. Only applies to date and number input types.',
+      control: { type: 'text' },
+      defaultValue: undefined,
+      table: {
+        category: 'Attributes: validation',
+        type: { summary: 'number | string' },
+        defaultValue: { summary: '-' }
+      }
+    },
+    max: {
+      description:
+        'The input’s maximum value. Only applies to date and number input types.',
+      control: { type: 'text' },
+      defaultValue: undefined,
+      table: {
+        category: 'Attributes: validation',
+        type: { summary: 'number | string' },
+        defaultValue: { summary: '-' }
+      }
+    },
+    step: {
+      description: 'Specifies the granularity that the value must adhere to.',
+      control: { type: 'text' },
+      defaultValue: undefined,
+      table: {
+        category: 'Attributes: Basic Input Behavior',
+        type: { summary: "number | 'any'" },
+        defaultValue: { summary: '-' }
+      }
+    },
+    autocapitalize: {
+      description:
+        'Controls whether and how text input is automatically capitalized.',
+      control: { type: 'select' },
+      options: ['off', 'none', 'on', 'sentences', 'words', 'characters'],
+      defaultValue: undefined,
+      table: {
+        category: 'Attributes: Form and Accessibility',
+        type: {
+          summary:
+            "'off' | 'none' | 'on' | 'sentences' | 'words' | 'characters'"
+        },
+        defaultValue: { summary: '-' }
+      }
+    },
+    autocorrect: {
+      description:
+        'Indicates whether the browser’s autocorrect feature is on or off.',
+      control: { type: 'select' },
+      options: ['off', 'on'],
+      defaultValue: undefined,
+      table: {
+        category: 'Attributes: Form and Accessibility',
+        type: { summary: "'off' | 'on'" },
+        defaultValue: { summary: '-' }
+      }
+    },
+    autocomplete: {
+      description:
+        'Specifies what permission the browser has to provide assistance in filling out form field values.',
+      control: { type: 'text' },
+      defaultValue: undefined,
+      table: {
+        category: 'Attributes: Basic Input Behavior',
+        type: { summary: 'string' },
+        defaultValue: { summary: '-' }
+      }
+    },
+    autofocus: {
+      description:
+        'Indicates that the input should receive focus on page load.',
+      control: { type: 'boolean' },
+      defaultValue: false,
+      table: {
+        category: 'Attributes: Basic Input Behavior',
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' }
+      }
+    },
+    enterkeyhint: {
+      description:
+        'Used to customize the label or icon of the enter key on virtual keyboards.',
+      control: { type: 'text' },
+      defaultValue: undefined,
+      table: {
+        category: 'Attributes: Form and Accessibility',
+        type: { summary: 'string' },
+        defaultValue: { summary: '-' }
+      }
+    },
+    inputmode: {
+      description:
+        'Specifies the type of data that is expected to be entered in the input.',
+      control: { type: 'select' },
+      options: [
+        'none',
+        'text',
+        'tel',
+        'url',
+        'email',
+        'numeric',
+        'decimal',
+        'search'
+      ],
+      defaultValue: undefined,
+      table: {
+        category: 'Attributes: Basic Input Behavior',
+        type: {
+          summary:
+            "'none' | 'text' | 'tel' | 'url' | 'email' | 'numeric' | 'decimal' | 'search'"
+        },
+        defaultValue: { summary: '-' }
+      }
+    },
+    spellcheck: {
+      description:
+        'Indicates whether the element is to have its spelling and grammar checked.',
+      control: { type: 'boolean' },
+      defaultValue: false,
+      table: {
+        category: 'Attributes: Form and Accessibility',
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'false' }
+      }
+    },
+    labelSlot: {
+      name: 'label',
+      description:
+        'The input’s label. Alternatively, you can use the `label` attribute.',
+      control: { type: 'text' },
+      table: {
+        category: 'Slots',
+        type: { summary: 'HTML' },
+        defaultValue: { summary: 'undefined' }
+      }
+    },
+    prefixSlot: {
+      name: 'prefix',
+      description:
+        'Used to prepend a presentational icon or similar element to the input.',
+      control: { type: 'text' },
+      table: {
+        category: 'Slots',
+        type: { summary: 'HTML' },
+        defaultValue: { summary: 'undefined' }
+      }
+    },
+    suffixSlot: {
+      name: 'suffix',
+      description:
+        'Used to append a presentational icon or similar element to the input.',
+      control: { type: 'text' },
+      table: {
+        category: 'Slots',
+        type: { summary: 'HTML' },
+        defaultValue: { summary: 'undefined' }
+      }
+    },
+    clearIconSlot: {
+      name: 'clear-icon',
+      description: 'An icon to use in lieu of the default clear icon.',
+      control: { type: 'text' },
+      table: {
+        category: 'Slots',
+        type: { summary: 'HTML' },
+        defaultValue: { summary: 'undefined' }
+      }
+    },
+    showPasswordIconSlot: {
+      name: 'show-password-icon',
+      description: 'An icon to use in lieu of the default show password icon.',
+      control: { type: 'text' },
+      table: {
+        category: 'Slots',
+        type: { summary: 'HTML' },
+        defaultValue: { summary: 'undefined' }
+      }
+    },
+    hidePasswordIconSlot: {
+      name: 'hide-password-icon',
+      description: 'An icon to use in lieu of the default hide password icon.',
+      control: { type: 'text' },
+      table: {
+        category: 'Slots',
+        type: { summary: 'HTML' },
+        defaultValue: { summary: 'undefined' }
+      }
+    },
+    helpTextSlot: {
+      name: 'help-text',
+      description:
+        'Text that describes how to use the input. Alternatively, you can use the `help-text` attribute.',
+      control: { type: 'text' },
+      table: {
+        category: 'Slots',
+        type: { summary: 'HTML' },
+        defaultValue: { summary: 'undefined' }
+      }
+    },
+    'ug-blur': {
+      description: 'Emitted when the control loses focus.',
+      table: {
+        category: 'Events',
+        type: { summary: 'CustomEvent' },
+        defaultValue: { summary: '-' }
+      }
+    },
+    'ug-change': {
+      description:
+        'Emitted when an alteration to the control’s value is committed by the user.',
+      table: {
+        category: 'Events',
+        type: { summary: 'CustomEvent' },
+        defaultValue: { summary: '-' }
+      }
+    },
+    'ug-clear': {
+      description: 'Emitted when the clear button is activated.',
+      table: {
+        category: 'Events',
+        type: { summary: 'CustomEvent' },
+        defaultValue: { summary: '-' }
+      }
+    },
+    'ug-focus': {
+      description: 'Emitted when the control gains focus.',
+      table: {
+        category: 'Events',
+        type: { summary: 'CustomEvent' },
+        defaultValue: { summary: '-' }
+      }
+    },
+    'ug-input': {
+      description: 'Emitted when the control receives input.',
+      table: {
+        category: 'Events',
+        type: { summary: 'CustomEvent' },
+        defaultValue: { summary: '-' }
+      }
+    },
+    'ug-invalid': {
+      description:
+        'Emitted when the form control has been checked for validity and its constraints aren’t satisfied.',
+      table: {
+        category: 'Events',
+        type: { summary: 'CustomEvent' },
+        defaultValue: { summary: '-' }
+      }
+    },
+    blur: {
+      description: 'Removes focus from the input.',
+      table: {
+        category: 'Methods',
+        type: { summary: 'void' },
+        defaultValue: { summary: '-' }
+      }
+    },
+    click: {
+      description: 'Simulates a click on the input.',
+      table: {
+        category: 'Methods',
+        type: { summary: 'void' },
+        defaultValue: { summary: '-' }
+      }
+    },
+    focus: {
+      description: 'Sets focus on the input.',
+      table: {
+        category: 'Methods',
+        type: { summary: 'void' },
+        defaultValue: { summary: '-' }
+      }
+    },
+    select: {
+      description: 'Selects all the text in the input.',
+      table: {
+        category: 'Methods',
+        type: { summary: 'void' },
+        defaultValue: { summary: '-' }
+      }
+    },
+    setCustomValidity: {
+      description:
+        'Sets a custom validation message for the input. Passing an empty string clears the message.',
+      table: {
+        category: 'Methods',
+        type: { summary: '(message: string) => void' },
+        defaultValue: { summary: '-' }
+      }
+    },
+    reportValidity: {
+      description:
+        "Checks the input's validity and displays the error message if invalid.",
+      table: {
+        category: 'Methods',
+        type: { summary: '() => boolean' },
+        defaultValue: { summary: '-' }
+      }
+    },
+    checkValidity: {
+      description:
+        "Checks the input's validity without displaying the error message.",
+      table: {
+        category: 'Methods',
+        type: { summary: '() => boolean' },
+        defaultValue: { summary: '-' }
+      }
+    }
+  }
 };
 
 export default meta;
@@ -12,7 +584,72 @@ export default meta;
 type Story = StoryObj;
 
 export const Input: Story = {
-  render: () => {
-    return html`<ug-input></ug-input>`;
+  args: {
+    type: 'text',
+    name: '',
+    value: '',
+    defaultValue: '',
+    size: 'medium',
+    filled: false,
+    pill: false,
+    label: '',
+    helpText: '',
+    clearable: false,
+    disabled: false,
+    placeholder: '',
+    readonly: false,
+    passwordToggle: false,
+    passwordVisible: false,
+    noSpinButtons: false,
+    form: '',
+    required: false,
+    pattern: '',
+    minlength: undefined,
+    maxlength: undefined,
+    min: undefined,
+    max: undefined,
+    step: undefined,
+    autocapitalize: 'off',
+    autocorrect: 'off',
+    autocomplete: '',
+    autofocus: false,
+    enterkeyhint: 'enter',
+    spellcheck: true,
+    inputmode: 'text'
+  },
+  render: (args) => {
+    return html` <ug-input
+      type="${args.type}"
+      name="${args.name}"
+      value="${args.value}"
+      default-value="${args.defaultValue}"
+      size="${args.size}"
+      ?filled="${args.filled}"
+      ?pill="${args.pill}"
+      label="${args.label}"
+      help-text="${args.helpText}"
+      ?clearable="${args.clearable}"
+      ?disabled="${args.disabled}"
+      placeholder="${args.placeholder}"
+      ?readonly="${args.readonly}"
+      ?password-toggle="${args.passwordToggle}"
+      ?password-visible="${args.passwordVisible}"
+      ?no-spin-buttons="${args.noSpinButtons}"
+      form="${args.form}"
+      ?required="${args.required}"
+      pattern="${args.pattern}"
+      minlength="${args.minlength}"
+      maxlength="${args.maxlength}"
+      min="${args.min}"
+      max="${args.max}"
+      step="${args.step}"
+      autocapitalize="${args.autocapitalize}"
+      autocorrect="${args.autocorrect}"
+      autocomplete="${args.autocomplete}"
+      ?autofocus="${args.autofocus}"
+      enterkeyhint="${args.enterkeyhint}"
+      ?spellcheck="${args.spellcheck}"
+      inputmode="${args.inputmode}"
+    ></ug-input>`;
   }
 };
