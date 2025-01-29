83099dd0db6b88f3a8159ea538d5f678a19316b2
mbardady <maryna.bardadym@ugent.be>
2025-01-29 17:50:09 +0100

    PROJ0484-61 stories/UGTab

--- stories/components/UGTab.stories.ts
+++ stories/components/UGTab.stories.ts
@@ -1,17 +1,36 @@
 import { html } from 'lit';
 import type { Meta, StoryObj } from '@storybook/web-components';
 import '/lib/components/tab';
+import '/lib/components/icon-button';
 import { action } from '@storybook/addon-actions';
+import { userEvent, within } from '@storybook/test';
 
 const meta: Meta = {
   title: 'Components/Tab',
   component: 'ug-tab',
 
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'Tabs are used inside [tab groups](?path=/docs/components-tabgroup--docs) to represent and activate [tab panels](?path=/docs/components-tabpanel--docs).'
+      },
+      source: {
+        format: true,
+        transform: (code: string) => {
+          // Remove empty/default attributes and replace boolean attributes from the source code display
+          return code;
+          //.replace(/\s(default-attribute="value")/g, '')
+          //.replace(/\s* attribute=""/g, ' attribute');
+        }
+      }
+    }
+  },
+
   argTypes: {
     panel: {
-      name: 'Panel',
       description:
-        'The name of the tab panel this tab is associated with. The panel must be located in the same tab group.',
+        'The name of the tab panel this tab is associated with. The panel must be located in the same tab group.<br>`reflects: true`',
       control: { type: 'text' }, // Input text box
       table: {
         type: { summary: 'string' },
@@ -19,8 +38,7 @@ const meta: Meta = {
       }
     },
     active: {
-      name: 'Active',
-      description: 'Draws the tab in an active state.',
+      description: 'Draws the tab in an active state.<br>`reflects: true`',
       control: { type: 'boolean' }, // Toggle switch
       table: {
         type: { summary: 'boolean' },
@@ -28,8 +46,8 @@ const meta: Meta = {
       }
     },
     closable: {
-      name: 'Closable',
-      description: 'Makes the tab closable and shows a close button.',
+      description:
+        'Makes the tab closable and shows a close button.<br>`reflects: true`',
       control: { type: 'boolean' }, // Toggle switch
       table: {
         type: { summary: 'boolean' },
@@ -37,8 +55,8 @@ const meta: Meta = {
       }
     },
     disabled: {
-      name: 'Disabled',
-      description: 'Disables the tab and prevents selection.',
+      description:
+        'Disables the tab and prevents selection.<br>`reflects: true`',
       control: { type: 'boolean' }, // Toggle switch
       table: {
         type: { summary: 'boolean' },
@@ -46,13 +64,32 @@ const meta: Meta = {
       }
     },
     updateComplete: {
-      name: 'Update Complete',
       description:
         'A read-only promise that resolves when the component has finished updating.',
       control: false, // Read-only, so no control
       table: {
         type: { summary: 'Promise<void>' }
       }
+    },
+    defaultSlot: {
+      name: 'default',
+      description: 'The tab’s label.',
+      table: {
+        type: { summary: 'HTML' },
+        defaultValue: { summary: 'None' }
+      },
+      control: { type: 'text' }
+    },
+    'ug-close': {
+      name: 'ug-close',
+      description:
+        'Emitted when the tab is closable and the close button is activated.',
+      table: {
+        type: { summary: 'CustomEvent' },
+        defaultValue: { summary: '-' }
+      },
+      action: 'ug-close',
+      control: false
     }
   }
 };
@@ -66,7 +103,8 @@ export const Tab: Story = {
     panel: '',
     active: false,
     closable: false,
-    disabled: false
+    disabled: false,
+    defaultSlot: 'Tab Title'
   },
   render: (args) => {
     return html` <ug-tab
@@ -74,7 +112,42 @@ export const Tab: Story = {
       ?active="${args.active}"
       ?closable="${args.closable}"
       ?disabled="${args.disabled}"
-    >
+      >${args.defaultSlot}
     </ug-tab>`;
+  }
+};
+
+export const TabWithEvents: Story = {
+  tags: ['!autodocs'],
+  args: {
+    panel: '',
+    active: false,
+    closable: true,
+    disabled: false,
+    defaultSlot: 'Tab Title'
+  },
+  render: (args) => {
+    return html` <ug-tab
+      @ug-close="${action('ug-close')}"
+      panel="${args.panel}"
+      ?active="${args.active}"
+      ?closable="${args.closable}"
+      ?disabled="${args.disabled}"
+      >${args.defaultSlot}
+    </ug-tab>`;
+  },
+  play: async ({ canvasElement }) => {
+    const tab = canvasElement.querySelector('ug-tab');
+    const iconButton = tab!.shadowRoot!.querySelector('ug-icon-button');
+    const closeButton = iconButton!.shadowRoot!.querySelector('button');
+
+    // Wait for the validation message to appear
+    await new Promise((resolve) => setTimeout(resolve, 500));
+
+    await userEvent.click(closeButton!);
+
+    await userEvent.click(tab!);
+
+    await userEvent.hover(tab!);
   }
 };
