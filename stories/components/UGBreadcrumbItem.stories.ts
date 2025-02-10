faa96b2ae60f746621759a9368547a4c4edb08d8
mbardady <maryna.bardadym@ugent.be>
2025-02-10 15:10:02 +0100

    PROJ0484-21 stories/UGBreadcrumbItem fix import problem

--- stories/components/UGBreadcrumbItem.stories.ts
+++ stories/components/UGBreadcrumbItem.stories.ts
@@ -1,6 +1,7 @@
 import { html } from 'lit';
 import type { Meta, StoryObj } from '@storybook/web-components';
 import '/lib/components/breadcrumb-item';
+import '/lib/components/breadcrumb';
 import '/lib/components/icon';
 import '/lib/components/icon-button';
 import { ifDefined } from 'lit/directives/if-defined.js';
