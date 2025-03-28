b78ecbbf2a21447359bc3641016c67a1e4cd3594
mbardady <maryna.bardadym@ugent.be>
2025-03-28 16:13:18 +0100

    PROJ0484-68 stories/UGTreeItem
    Change subtitle to description for the link
    Add events
    Add TreeItemWithEvents

--- stories/components/UGTreeItem.stories.ts
+++ stories/components/UGTreeItem.stories.ts
@@ -1,17 +1,24 @@
 import { html } from 'lit';
 import type { Meta, StoryObj } from '@storybook/web-components';
+import { action } from '@storybook/addon-actions';
+import { userEvent, within } from '@storybook/test';
 import '/lib/components/tree-item';
+import '/lib/components/tree';
 import '/lib/components/checkbox';
 import '/lib/components/icon';
 import '/lib/components/spinner';
+import { UgTreeItem } from '@ugent-ui/core/components/tree-item';
+import { UgIcon } from '@ugent-ui/core/components/icon';
 
 const meta: Meta = {
   title: 'Components/TreeItem',
   component: 'tree-item',
   parameters: {
     docs: {
-      subtitle:
-        'A tree item serves as a hierarchical node that lives inside a tree. You can read more about it in [the documentation of a Tree](?path=/docs/components-tree--docs)',
+      description: {
+        component:
+          'A tree item serves as a hierarchical node that lives inside a tree. You can read more about it in [the documentation of a Tree](?path=/docs/components-tree--docs)'
+      },
       importSection: true, // Enables the import section
       dependencies: ['icon', 'checkbox', 'spinner']
     }
@@ -77,6 +84,75 @@ const meta: Meta = {
       description: 'The icon to show when the tree item is collapsed.',
       table: {
         category: 'Slots'
+      }
+    },
+    ugExpand: {
+      control: false,
+      name: 'ug-expand',
+      action: 'ug-expand',
+      description: 'Emitted when the tree item expands.',
+      table: {
+        category: 'Events',
+        type: { summary: 'CustomEvent' },
+        defaultValue: { summary: undefined }
+      }
+    },
+    ugAfterExpand: {
+      control: false,
+      name: 'ug-after-expand',
+      action: 'ug-after-expand',
+      description:
+        'Emitted after the tree item expands and all animations are complete.',
+      table: {
+        category: 'Events',
+        type: { summary: 'CustomEvent' },
+        defaultValue: { summary: undefined }
+      }
+    },
+    ugCollapse: {
+      control: false,
+      name: 'ug-collapse',
+      action: 'ug-collapse',
+      description: 'Emitted when the tree item collapses.',
+      table: {
+        category: 'Events',
+        type: { summary: 'CustomEvent' },
+        defaultValue: { summary: undefined }
+      }
+    },
+    ugAfterCollapse: {
+      control: false,
+      name: 'ug-after-collapse',
+      action: 'ug-after-collapse',
+      description:
+        'Emitted after the tree item collapses and all animations are complete.',
+      table: {
+        category: 'Events',
+        type: { summary: 'CustomEvent' },
+        defaultValue: { summary: undefined }
+      }
+    },
+    ugLazyChange: {
+      control: false,
+      name: 'ug-lazy-change',
+      action: 'ug-lazy-change',
+      description: 'Emitted when the tree item’s lazy state changes.',
+      table: {
+        category: 'Events',
+        type: { summary: 'CustomEvent' },
+        defaultValue: { summary: undefined }
+      }
+    },
+    ugLazyLoad: {
+      control: false,
+      name: 'ug-lazy-load',
+      action: 'ug-lazy-load',
+      description:
+        'Emitted when a lazy item is selected. Use this event to asynchronously load data and append items to the tree before expanding. After appending new items, remove the lazy attribute to remove the loading state and update the tree.',
+      table: {
+        category: 'Events',
+        type: { summary: 'CustomEvent' },
+        defaultValue: { summary: undefined }
       }
     },
     'getChildrenItems()': {
@@ -281,4 +357,71 @@ export const LazyLoading: Story = {
       });
     </script>
   `
+};
+
+export const TreeItemWithEvents: Story = {
+  render: (args) => html`
+    <ug-tree>
+      <ug-tree-item id="parent" expanded>
+        Parent Item
+        <ug-tree-item id="child">Child Item</ug-tree-item>
+      </ug-tree-item>
+      <ug-tree-item
+        lazy
+        @ug-expand="${action('ug-expand')}"
+        @ug-after-expand="${action('ug-after-expand')}"
+        @ug-collapse="${action('ug-collapse')}"
+        @ug-after-collapse="${action('ug-after-collapse')}"
+        @ug-lazy-change="${action('ug-lazy-change')}"
+        @ug-lazy-load="${action('ug-lazy-load')}"
+        >Available Trees</ug-tree-item
+      >
+    </ug-tree>
+  `,
+  play: async ({ canvasElement }) => {
+    const lazyItem: UgTreeItem = document.querySelector('ug-tree-item[lazy]')!;
+    lazyItem.addEventListener('ug-lazy-load', () => {
+      // Simulate asynchronous loading
+      setTimeout(() => {
+        const subItems = ['Birch', 'Cedar', 'Maple', 'Pine'];
+        for (const item of subItems) {
+          const treeItem = document.createElement('ug-tree-item');
+          treeItem.innerText = item;
+          lazyItem.append(treeItem);
+        }
+        // Disable lazy mode once the content has been loaded
+        lazyItem.lazy = false;
+      }, 2000);
+    });
+
+    await new Promise((resolve) => setTimeout(resolve, 100));
+
+    const canvas = within(canvasElement);
+    const parentItem = await canvas.getByText('Parent Item');
+    const expandIcon: UgIcon = parentItem
+      .closest('ug-tree-item')
+      ?.shadowRoot?.querySelector('ug-icon')!;
+
+    // Simulate collapsing the parent item
+    await userEvent.click(expandIcon);
+
+    await new Promise((resolve) => setTimeout(resolve, 100));
+
+    // Simulate expanding the parent item again
+    await userEvent.click(expandIcon);
+
+    const lazyExpandIcon: UgIcon = (await canvas.getByText('Available Trees'))
+      .closest('ug-tree-item')
+      ?.shadowRoot?.querySelector('ug-icon')!;
+
+    await new Promise((resolve) => setTimeout(resolve, 100));
+
+    //Simulate clicking on lazy item
+    await userEvent.click(lazyExpandIcon);
+
+    await new Promise((resolve) => setTimeout(resolve, 100));
+
+    //Close lazy item
+    await userEvent.click(lazyExpandIcon);
+  }
 };
